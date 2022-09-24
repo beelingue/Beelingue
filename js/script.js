2c02d64a13b7600a63037032f82b49afd4274a02
André Santana <andresanlopes@gmail.com>
2022-09-23 23:22:44 -0300

    23/09 - Edições Gerais
    Combinação da branch com a página login
    Ajuste dos diretórios para arquivos .JS e .CSS

---
+++ js/script.js
@@ -0,0 +1,42 @@
+function redirecioneComHistorico() {
+  // Faz um redirecionamento sem adicionar a página original ao histórico de navegação do browser.
+  window.location.href = "$";
+}
+
+function redirecioneComHistorico2() {
+  // Faz um redirecionamento sem adicionar a página original ao histórico de navegação do browser.
+  window.location.href = "$$";
+}
+
+function redirecioneComHistorico3() {
+  // Faz um redirecionamento sem adicionar a página original ao histórico de navegação do browser.
+  window.location.href = "$$$";
+}
+
+let arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++) {
+  arrow[i].addEventListener("click", (e)=>{
+ let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+ arrowParent.classList.toggle("showMenu");
+  });
+}
+
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+console.log(sidebarBtn);
+sidebarBtn.addEventListener("click", ()=>{
+  sidebar.classList.toggle("close");
+});
+
+//Barra de progresso
+$(document).ready(function(){
+  $(".barra_prog").css("width", window.pageYOffset + "%");
+  function map(currentA, maxA , maxB ){
+    return (currentA * maxB) / maxA;
+  }
+  //on scroll event
+  $(window).scroll(function(){
+    var pageMaxY = $(document).height() - $(window).height();
+    $(".barra_prog").css("width", map(window.pageYOffset, pageMaxY,100) + "%");
+  });
+});
