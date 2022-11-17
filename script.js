50830889919056f05a98ad4c9c96fbe163c88c3b
André Santana <andresanlopes@gmail.com>
2022-11-17 15:01:48 -0300

    17/11 - Edições Gerais
    → Correções:
    
    - borda para botão "voltar ao topo";
    - conteúdo editado;
    - seção "Sobre nós" com espaçamento correto;

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const menuSand = menuHamburguer.querySelector(".sand")
 menuSand.addEventListener("click", () =>{
 
     //IMPEDE A ROLAGEM DA PÁGINA APÓS O CLICK NO MENU
-    document.body.style.overflow = show ? "hidden" : "initial"
+    //document.body.style.overflow = show ? "hidden" : "initial"
     //document.barra.style.display = show ? "none" : "block"
 
     menuHamburguer.classList.toggle("click", show)
